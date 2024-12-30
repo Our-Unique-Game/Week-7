2b6332c1e0b639c2f46448d72952cd2f1fce0a07
Roi Bruchim <roibr23@gmail.com>
2024-12-30 14:58:30 +0200

    update_swithch

--- Assets/Scripts/3-objects/ToggleLamp.cs
+++ Assets/Scripts/3-objects/ToggleLamp.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
-public class ToggleLamp : MonoBehaviour
+public class LampSwitchManager : MonoBehaviour
 {
-    public Light lampLight; // Reference to the Light component
+    public Light switchLamp; // The lamp that acts as the switch
+    public List<Light> otherLamps = new List<Light>(); // List of lamps controlled by the switch
+    public float activationRange = 5f; // Range within which the switch can be toggled
+    public Transform player; // Reference to the player
 
-    // Action for toggling light
     private InputAction toggleLightAction;
 
     private void OnEnable()
     {
-        // Create a new Input Action
-        toggleLightAction = new InputAction("ToggleLight", binding: "<Keyboard>/e");
+        // Create a new Input Action for the "E" key
+        toggleLightAction = new InputAction("ToggleSwitch", binding: "<Keyboard>/e");
         toggleLightAction.Enable();
-        
-        // Bind the action to a method
-        toggleLightAction.performed += ToggleLight;
+
+        // Bind the action to the ToggleSwitchLamp method
+        toggleLightAction.performed += TryToggleSwitchLamp;
     }
 
     private void OnDisable()
@@ -23,11 +26,31 @@ public class ToggleLamp : MonoBehaviour
         toggleLightAction.Disable();
     }
 
-    private void ToggleLight(InputAction.CallbackContext context)
+    private void TryToggleSwitchLamp(InputAction.CallbackContext context)
     {
-        if (lampLight != null)
+        // Check if the player is within range of the switch lamp
+        if (switchLamp != null && player != null)
         {
-            lampLight.enabled = !lampLight.enabled;
+            float distance = Vector3.Distance(player.position, switchLamp.transform.position);
+            if (distance <= activationRange)
+            {
+                ToggleSwitchLamp();
+            }
+        }
+    }
+
+    private void ToggleSwitchLamp()
+    {
+        // Toggle the switch lamp's state
+        switchLamp.enabled = !switchLamp.enabled;
+
+        // Set all other lamps to match the state of the switch lamp
+        foreach (Light lamp in otherLamps)
+        {
+            if (lamp != null)
+            {
+                lamp.enabled = switchLamp.enabled;
+            }
         }
     }
 }
